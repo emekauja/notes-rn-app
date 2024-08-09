8b8f22c6088d50efc42a05ca872828857e1fce1a
emekauja <ujadninth@gmail.com>
2024-08-09 17:21:52 +0100

    added create route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -42,6 +42,7 @@ export default function RootLayout() {
           <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
             <Stack.Screen name="+not-found" />
+            <Stack.Screen name="create" options={{ headerShown: false }} />
           </Stack>
         </ThemeProvider>
       </PersistGate>
